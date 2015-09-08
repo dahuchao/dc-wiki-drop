624ac72ed740df9a64ced45117c066fede227e45
dahuchao <dahuchao.github@laposte.net>
2015-09-08 15:26:35 +0200

    Gestion des documents

--- public/dc-wiki-dropbox.js
+++ public/dc-wiki-dropbox.js
@@ -1,14 +1,14 @@
 'use strict';
 
 //Pages service used to communicate Pages REST endpoints
-angular.module('dcWiki', ['ngSanitize', 'ui.router', 'ngResource', 'ngCookies'])
+angular.module('dcWiki', ['angularFileUpload', 'ngSanitize', 'ui.router', 'ngResource', 'ngCookies'])
 
 /** 
  * Configuration des routes de l'application
  */
 .config(function ($stateProvider, $urlRouterProvider) {
     //$urlRouterProvider.otherwise("/connexion");
-    //$urlRouterProvider.when("/", "/pages/homepage");
+    $urlRouterProvider.when("/", "/pages/homepage");
     $urlRouterProvider.when("/pages", "/pages/homepage");
     $stateProvider
         .state("connexion", {
@@ -17,14 +17,25 @@ angular.module('dcWiki', ['ngSanitize', 'ui.router', 'ngResource', 'ngCookies'])
             controller: "dcWikiConnexion"
         })
         .state("wiki", {
-            url: "/",
+            url: "/"
+                /*,
+                            templateUrl: "page.html",
+                            controller: "dcPageController"*/
+        })
+        .state("televersement", {
+            url: "/pages/{page}/televersement",
+            templateUrl: "televersement.html",
+            controller: "dcTeleversementController"
+        })
+        .state("pages", {
+            url: "/pages/{page}",
             templateUrl: "page.html",
             controller: "dcPageController"
         })
-        .state("wiki.pages", {
-            url: "pages/:page",
-            templateUrl: "page.html",
-            controller: "dcPageController"
+        .state("docs", {
+            url: "/docs/{doc}",
+            templateUrl: "doc.html",
+            controller: "dcDocController"
         });
     //$locationProvider.html5Mode(true);
 })
@@ -156,6 +167,111 @@ angular.module('dcWiki', ['ngSanitize', 'ui.router', 'ngResource', 'ngCookies'])
         }
     }])
 
+/**
+ * Service pilote du service dropbox
+ */
+.service("DocumentsService", ['$resource', 'IdentificationService',
+    function ($resource, IdentificationService) {
+        function telecharger(nomDocument, traitementDocument, traitementErreur) { // Calcul du jeton de controle d'accès
+            var token = IdentificationService.getToken();
+            // URL du service REST de dropbox
+            var urlDropbox = 'https://api-content.dropbox.com/1/files/auto';
+            // Chemin du dossier des pages dans dropbox
+            var dossierDropbox = urlDropbox + '/PersonalWiki/Wiki_Documents';
+            // Ressource des pages du wiki
+            var Documents = $resource(dossierDropbox + '/:document', {
+                document: '@document'
+            }, {
+                get: {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': token
+                    }
+                }
+            });
+            // Journalisation
+            console.log('* Lecture du document : ' + nomDocument);
+            // Appel de la page
+            Documents.get({
+                document: nomDocument
+            }, function (contenu) {
+                traitementDocument(contenu);
+            }, function (reason) {
+                traitementErreur(reason);
+            });
+        }
+
+        function resoudreLien(nomDocument, traitementDocument, traitementErreur) {
+            var token = IdentificationService.getToken();
+            // URL du service REST de dropbox
+            var urlDropbox = 'https://api.dropbox.com/1/shares/auto';
+            // Chemin du dossier des pages dans dropbox
+            var dossierDropbox = urlDropbox + '/PersonalWiki/Wiki_Documents';
+            // Ressource des pages du wiki
+            var Documents = $resource(dossierDropbox + '/:document', {
+                document: '@document'
+            }, {
+                post: {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': token
+                    }
+                }
+            });
+            // Journalisation
+            console.log('* Lecture du document : ' + nomDocument);
+            // Appel de la page
+            Documents.post({
+                document: nomDocument
+            }, function (lien) {
+                traitementDocument(lien);
+            }, function (reason) {
+                traitementErreur(reason);
+            });
+        }
+        return {
+            resoudreLien: resoudreLien,
+            telecharger: telecharger
+        }
+    }])
+
+// Controleur des pages de wiki
+.controller('dcDocController', ['DocumentsService', '$state', '$scope',
+    function (DocumentsService, $state, $scope) {
+        // Calcul du nom du document demandé
+        var nomDoc = encodeURI($state.params.doc);
+        // Si le nom du document est bien définie
+        if (!nomDoc.match("undefined")) {
+            // Journalisation
+            console.log('* Lecture du document : ' + nomDoc);
+            // Téléchargement de la page
+            DocumentsService.resoudreLien(nomDoc, function (lien) {
+                // Journalisation
+                console.log('* Document : ' + lien.url);
+                // Ouverture du document
+                $scope.url = lien.url;
+            }, function (reason) {
+                if (reason.status === 404) {
+                    // Journalisation
+                    console.log('* Erreur : nom du document indéfinit.');
+                }
+                if (reason.status === 401) {
+                    // Changement d'état pour ouvrir le wiki
+                    $state.go('connexion');
+                } else {
+                    // Journalisation
+                    console.log('* Erreur : nom du document indéfinit.');
+                }
+            });
+        }
+    }])
+
+// Controleur des pages de wiki
+.controller('dcTeleversementController', ['FileUploader', '$state', '$scope',
+    function (FileUploader, $state, $scope) {
+        $scope.uploader = new FileUploader();
+    }])
+
 // Controleur des pages de wiki
 .controller('dcPageController', ['$state', '$scope', 'PagesService', 'dcWikiFormateur',
     function ($state, $scope, PagesService, dcWikiFormateur) {
@@ -184,6 +300,12 @@ angular.module('dcWiki', ['ngSanitize', 'ui.router', 'ngResource', 'ngCookies'])
                 if (reason.status === 404) {
                     $scope.pagecontenu = "+ " + nomPage;
                     $scope.edition = true;
+                }
+                if (reason.status === 401) {
+                    $scope.pagehtml = "erreur";
+                    $scope.edition = false;
+                    // Changement d'état pour ouvrir le wiki
+                    $state.go('connexion');
                 }
                 // Une erreur s'est produite
                 $scope.pagehtml = "erreur";
