adf14c46ac3b0788cfcfdf5bc52242d7c5de10aa
dahuchao <dahuchao.github@laposte.net>
2018-05-01 20:00:13 +0200

    passage par attribut static

--- client/src/app/page/page.component.ts
+++ client/src/app/page/page.component.ts
@@ -2,10 +2,19 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({selector: 'app-page', templateUrl: './page.component.html', styleUrls: ['./page.component.css']})
 export class PageComponent implements OnInit {
-  edition : boolean = false
+  edition : boolean = true
+  page : string = `Il ne faut pas *vendre* la _peau_ de l 'ourse =avant= 
+d' avoir crier victoire 
+trop tot !!Tel est pris qui rira * bien * le _dernier_
+ ! C 'est la cerise sur le pompom ! C'est l'étincelle qui 
+fait déborder le vase ! C'est la goutte d'eau qui met le feu
+aux poudres ! Petit à petit l'oiseau devient forgeron ! Les 
+ciseaux à bois : Les [chiens] aussi.
+`
   constructor() {}
 
   ngOnInit() {}
+  
   editer() {
     this.edition = !this.edition
   }
