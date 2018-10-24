b32575df0cc3332dfce30487ef1c6e4e4b632ec9
dahuchao <dahuchao.github@laposte.net>
2018-10-24 23:02:02 +0200

    inner html

--- client/src/page/page.lit.js
+++ client/src/page/page.lit.js
@@ -5,22 +5,14 @@ import marked from "marked"
 
 export default etat => html`
 <div id="page">
-${etat
-  .page
-  .map(page => {
-    console.log(`page: ${page}`)
-    const htm = marked(page);
-    const texte = (h) => directive((part) => {
-      part.innerHtml = h;
-    });
-    console.log(`page: ${htm}`)
-    return etat.edition
-        ? html`<textarea rows="15" cols="50">${page}</textarea>`
-        : html`<div id="page">${htm}</div>
-        <script>
-          var elem = document.getElementById("page");
-          elem.innerHTML = ${htm};
-        </script>`
+  ${etat
+    .page
+    .map(page => {
+      const divPage = document.createElement('div');
+      divPage.innerHTML = marked(page);
+      return etat.edition
+          ? html`<textarea>${page}</textarea>`
+          : html`<div>${divPage}</div>`
     })
   }
 </div>
--- client/src/page/page.md.js
+++ client/src/page/page.md.js
@@ -1,5 +1,4 @@
 export default `
-
 # Client
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.0.
@@ -8,5 +7,4 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 To get more help on the Angular CLI use ng help or 
 go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
-
 `
