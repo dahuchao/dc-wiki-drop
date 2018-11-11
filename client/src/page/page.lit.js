1ecffefa018a1a2874ab2e81a37602f097335514
dahuchao <dahuchao.github@laposte.net>
2018-11-11 21:36:45 +0100

    implementation de la date de mise à jour

--- client/src/page/page.lit.js
+++ client/src/page/page.lit.js
@@ -36,7 +36,15 @@ export default etat => {
             <a @click="${e => {cmd$.next({type: "SUR_ENREGISTRER", page: document.getElementById("textPage").value})}}" class="waves-effect waves-light btn">Enregistrer</a>
             <a @click="${e => cmd$.next({type: "SUR_ANNULER"})}" class="waves-effect waves-light btn">Annuler</a>
           </div>`
-        : null
+        : html`<p id="maj">${Array(etat.page.dateMaj).map(enDate)}</p>`
       }
     </div>`
 } 
+
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const enDate = date => {
+  const d = new Date(date)
+  const j = d.toLocaleDateString('fr-FR', options)
+  const t = d.toLocaleTimeString('fr-FR')
+  return `Mise à jour: ${j} à ${t}`
+}
